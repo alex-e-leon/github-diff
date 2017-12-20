f772befc342e79b83d1aff544ff04e47251caf05
Ben Patterson <ben.patterson@domain.com.au>
2017-12-20 16:44:42 +1100

    Switch to GitHub data API for files that are too large for the content API

--- index.js
+++ index.js
@@ -36,31 +36,31 @@ function buildHeader(fileA, fileB) {
 }
 
 function buildContent(github, owner, repo, base, head, file) {
-  const {filename, patch, status} = file;
+  const {filename, patch, status, sha} = file;
 
   // Get the content for the files
   switch (status) {
     case 'removed':
-      return getContent(github, owner, repo, filename, base).then(content => {
+      return getContent(github, owner, repo, filename, base, sha).then(content => {
         return {filename, patch, status, header: buildHeader(filename, filename), fileA: atob(content)};
       });
 
     case 'added':
-      return getContent(github, owner, repo, filename, head).then(content => {
+      return getContent(github, owner, repo, filename, head, sha).then(content => {
         return {filename, patch, status, header: buildHeader(filename, filename), fileB: atob(content)};
       });
 
     case 'modified':
       return Promise.all([
-          getContent(github, owner, repo, filename, base),
-          getContent(github, owner, repo, filename, head),
+          getContent(github, owner, repo, filename, base, sha),
+          getContent(github, owner, repo, filename, head, sha),
       ]).then(files => {
         const [fileA, fileB] = files;
         return {filename, patch, status, header: buildHeader(filename, filename), fileA: atob(fileA), fileB: atob(fileB)};
       });
 
     case 'renamed':
-      return getContent(github, owner, repo, filename, head).then(content => {
+      return getContent(github, owner, repo, filename, head, sha).then(content => {
         const decodedFile = atob(content);
         const previousFilename = file.previous_filename;
         const header = buildHeader(filename, previousFilename);
@@ -75,7 +75,7 @@ function buildContent(github, owner, repo, base, head, file) {
   }
 }
 
-function getContent(github, owner, repo, path, commit) {
+function getContent(github, owner, repo, path, commit, sha) {
   return github.repos.getContent({
     owner,
     repo,
@@ -83,6 +83,18 @@ function getContent(github, owner, repo, path, commit) {
     ref: commit,
   }).then((res) => {
     return res.content;
+  }).catch((err) => {
+    try {
+      const apiError = JSON.parse(err);
+      if (apiError.errors.find(error => error.code === 'too_large')) {
+        return github.gitdata.getBlob({
+          owner,
+          repo,
+          sha
+        }).then(data => data.content);
+      }
+    } catch(parseError) {}
+    return Promise.reject(err);
   }).catch((err) => {
     throw new Error(`Unable to get content for ${path} @commit:${commit}. ${err}`);
   });
